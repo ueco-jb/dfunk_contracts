96630aa0700beb8fcc9389bb3c0e9fca46843b80
Jakub Bogucki <software-solutions@tuta.io>
2022-06-28 13:02:04 +0200

    Add MigrateMsg

--- contracts/luna-distributor/src/msg.rs
+++ contracts/luna-distributor/src/msg.rs
@@ -3,6 +3,9 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub burn_address: String,
     pub whitelist: Vec<Whitelist>,
